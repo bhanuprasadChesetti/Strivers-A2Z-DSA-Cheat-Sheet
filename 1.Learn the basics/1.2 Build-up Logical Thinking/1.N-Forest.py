225c960adaf752446d38efd31688b536552f2f7e
Bhanuprasad Chesetti <chesettibhanuprasad@gmail.com>
2024-01-30 08:27:22 +0530

    Solved N-Pattern problem

---
+++ 1.Learn the basics/1.2 Build-up Logical Thinking/1.N-Forest.py
@@ -0,0 +1,20 @@
+
+
+# N-Forest
+'''
+    link: https://www.codingninjas.com/studio/problems/n-forest_6570177
+'''
+
+
+def nForest(n:int) ->None:
+
+    # Iterate through each row
+    for _ in range(n):
+
+        # To print each row
+        for _ in range(n):
+            print('*',end=' ')
+        
+        print('\n')
+
+
